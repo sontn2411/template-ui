82dd4bbb3abdeca600e67a8726a8b4c6222090e7
Tran Ngoc Son <son.tran@sweetsoft.vn>
2025-11-21 10:46:02 +0700

    update icon svg animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,119 @@
 import './App.css'
-import { mainTako } from './assets'
+import {
+  foodAruku,
+  foodHashi,
+  foodTako,
+  foodTamanegi,
+  mainAnago,
+  mainHana,
+  mainTako,
+  mainTamanegi,
+  mainUnni,
+  playInu,
+  playKi,
+  playKirakiral,
+  playKumo,
+  playTaiyou,
+} from './assets'
+
+const dataArray = [
+  {
+    id: 1,
+    name: 'Tako',
+    image: mainTako,
+    className: 'w-[138px] h-[151px]  tako-animate',
+  },
+  {
+    id: 2,
+    name: 'Unni',
+    image: mainUnni,
+    className: 'w-[340px] h-[200px]  tuni-animate',
+  },
+  {
+    id: 3,
+    name: 'Hana',
+    className: 'w-[160px] h-[104px] hana-animate',
+    image: mainHana,
+  },
+  {
+    id: 4,
+    name: 'Anago',
+    className: 'w-[180px] h-[100px] anago-animate',
+    image: mainAnago,
+  },
+  {
+    id: 5,
+    name: 'Tamanegi',
+    className: 'w-[93px] h-[114px] tamanegi-animate ',
+    image: mainTamanegi,
+  },
+  {
+    id: 6,
+    name: 'Food Hashi',
+    className: 'w-[110px] h-[120px] food-Hashi-animate',
+    image: foodHashi,
+  },
+  {
+    id: 7,
+    name: 'Food Tamanegi',
+    className: 'w-[71px] h-[59px] food-Tamanegi-animate',
+    image: foodTamanegi,
+  },
+  {
+    id: 8,
+    name: 'Food Tako',
+    className: 'w-[82px] h-[74px] food-Tako-animate',
+    image: foodTako,
+  },
+  {
+    id: 9,
+    name: 'Food Aruku',
+    className: 'w-[372px] h-[77px] food-Aruku-animate',
+    image: foodAruku,
+  },
+  {
+    id: 10,
+    name: 'Play Kumo',
+    className: 'w-[196px] h-[52px] play-Kumi-animate',
+    image: playKumo,
+  },
+  {
+    id: 11,
+    name: 'Play Kirakiral',
+    className: 'w-[229px] h-[125px] play-Kirakiral-animate',
+    image: playKirakiral,
+  },
+  {
+    id: 12,
+    name: 'Play Inu',
+    className: 'w-[51px] h-[34px] play-inu-animate ',
+    image: playInu,
+  },
+  {
+    id: 13,
+    name: 'Play Ki',
+    className: 'w-[100px] h-[110px] play-ki-animate ',
+    image: playKi,
+  },
+  {
+    id: 12,
+    name: 'Play taiyou',
+    className: 'w-[71px] h-[70px] play-taiyou-animate ',
+    image: playTaiyou,
+  },
+]
 
 function App() {
   return (
-    <div>
-      <div
-        className='w-[138px] h-[151px] bg-no-repeat tako-animate '
-        style={{
-          backgroundImage: `url(${mainTako})`,
-        }}
-      ></div>
+    <div className='flex flex-wrap gap-10'>
+      {dataArray.map((item) => (
+        <div key={item.id} className=''>
+          <div
+            className={`${item.className} bg-no-repeat`}
+            style={{ backgroundImage: `url(${item.image})` }}
+          ></div>
+        </div>
+      ))}
     </div>
   )
 }
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,31 @@
 import mainTako from './images/main_tako.png'
+import mainUnni from './images/main_umi.png'
+import mainHana from './images/main_hana.png'
+import mainAnago from './images/main_anago.png'
+import mainTamanegi from './images/main_tamanegi.png'
+import foodHashi from './images/food_hashi.png'
+import foodTamanegi from './images/food_tamanegi.png'
+import foodTako from './images/food_tako.png'
+import foodAruku from './images/food_aruku.png'
+import playKumo from './images/play_kumo.png'
+import playKirakiral from './images/play_kirakiraL.png'
+import playInu from './images/play_inu.png'
+import playKi from './images/play_ki.png'
+import playTaiyou from './images/play_taiyou.png'
 
-export { mainTako }
+export {
+  foodTamanegi,
+  mainTako,
+  mainUnni,
+  mainHana,
+  mainAnago,
+  mainTamanegi,
+  foodHashi,
+  foodTako,
+  foodAruku,
+  playKumo,
+  playKirakiral,
+  playInu,
+  playKi,
+  playTaiyou,
+}
